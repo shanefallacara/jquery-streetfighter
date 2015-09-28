9987e4f96733632a45a75620df45c3bcef1647f1
shanefallacara <shanefallacara@gmail.com>
2015-09-27 20:21:39 -0400

    minor change

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
 	$('.ryu').mouseenter(function() {
    		$('.ryu-still').hide();
     	$('.ryu-ready').show();
-  })
+  	})
 	.mouseleave (function() {
 		$('.ryu-still').show();
     	$('.ryu-ready').hide();
@@ -15,16 +15,15 @@ $(document).ready(function () {
     		{'left': '1020px'}, 500,function() {
     		$(this).hide();
     		$(this).css('left', '570px');
-    	}
-    );	
+    	});	
 	})
 	.mouseup (function() {
 		$('.ryu-ready').show();
     	$('.ryu-throwing').hide();
-	});
-	function playHadouken () {
+		});
+});
+function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
-});
